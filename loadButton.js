667b605bff066841ebae4ef985b084f2ae43c8b8
neyaz14 <neyazmorshid@gmail.com>
2024-10-07 00:58:17 +0600

    1.modal from details 2. wishlist 3. loading spinner 4. sort by price 4. active adopt button with modal

--- loadButton.js
+++ loadButton.js
@@ -9,12 +9,32 @@ const LoadButtonCategories = ()=>{
 LoadButtonCategories();
 
 
+
+
+
+const removeActiveClass=()=>{
+    const classname = document.getElementsByClassName('active-btn-class');
+    // ei class name theke ekta arry-like ase jar protita elements ke normally for calay access korte hobe
+    // classname.classList.remove('active');
+    for(const btn of classname){
+        btn.classList.remove('active');
+    }
+
+
+}
+
 // to show pets by category
 const loadbycategory=(id)=>{
 
     fetch(`https://openapi.programming-hero.com/api/peddy/category/${id}`)
     .then(res=> res.json())
-    .then(data => showAllPets(data.data))
+    .then(data => {
+        removeActiveClass();
+        const activebtn = document.getElementById(`btn-${id}`); 
+        activebtn.classList.add('active');
+        console.log(activebtn);
+        showAllPets(data.data)
+    })
     .catch(error => console.log(error));
 }
 
@@ -27,7 +47,7 @@ const LoadPetsInfoForBtn = (pets) =>{
         const div = document.createElement('div');
         // button onclick function --  
         div.innerHTML= `
-        <button id="btn-${pet.category}" onclick="loadbycategory('${pet.category}')" class="active-btn-class btn btn-outline hover:rounded-full  text-black font-bold category-btn h-[69px] w-[139px]">
+        <button id="btn-${pet.category}" onclick="loadbycategory('${pet.category}')" class="active-btn-class btn btn-outline hover:rounded-full  text-black font-bold category-btn  h-[73px] w-[143px]">
         <img src=${pet.category_icon}> ${pet.category}
         </button>
         `
